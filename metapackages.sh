9c18985ec122adbb9c5d583b840ff7d448d93fd3
ax75 <ax75.senshi@gmail.com>
2021-05-30 11:53:21 -0400

    Fixed some bugs
    Added Strict root mode.

--- metapackages.sh
+++ metapackages.sh
@@ -101,102 +101,107 @@ function apt(){
 
 #Function which calls and manages the whole tool.
 function main(){
-	sudo ls
-        Menu=(
-        'Systen'
-        'Desktop-environments/Window-managers'
-        "Tools"
-        "Tool-Category"
-        "Install-Top-10-Tools"
-        "Install-Above-All"
-        "Help"
-	"INSTALL"
-	"Exit"
-        )
-        System=(
-	"kali-linux-core"
-	"kali-linux-headless"
-	"kali-linux-default"
-	"kali-linux-light"
-	"kali-linux-arm"
-	"kali-linux-nethunter"
-	"Back"
-	)
-	Desktop=(
-	"kali-desktop-core"
-	"kali-desktop-e17"
-	"kali-desktop-gnome"
-	"kali-desktop-i3"
-	"kali-desktop-kde"
-	"kali-desktop-lxde"
-	"kali-desktop-mate"
-	"kali-desktop-xfce"
-	"Back"	
-	)
-	Tools=(
-	"kali-tools-gpu"
-	"kali-tools-hardware"
-	"kali-tools-crypto-stego"
-	"kali-tools-fuzzing"
-	"kali-tools-802-11"
-	"kali-tools-bluetooth"
-	"kali-tools-rfid"
-	"kali-tools-sdr"
-	"kali-tools-voip"
-	"kali-tools-windows-resources"
-	"Back"
-	)
-	CateTools=(
-	"kali-tools-information-gathering"
-	"kali-tools-vulnerability"
-	"kali-tools-web"
-	"kali-tools-database"
-	"kali-tools-passwords"
-	"kali-tools-wireless"
-	"kali-tools-reverse-engineering"
-	"kali-tools-exploitation"
-	"kali-tools-social-engineering"
-	"kali-tools-sniffing-spoofing"
-	"kali-tools-post-exploitation"
-	"kali-tools-forensics"
-	"kali-tools-reporting"	
-	"Back"
-	)
+	if [[ $(whoami) == "root"  ]]; then
+        	Menu=(
+        	'Systen'
+        	'Desktop-environments/Window-managers'
+        	"Tools"
+        	"Tool-Category"
+        	"Install-Top-10-Tools"
+        	"Install-Above-All"
+        	"Help"
+		"INSTALL"
+		"Exit"
+        	)
+        	System=(
+		"kali-linux-core"
+		"kali-linux-headless"
+		"kali-linux-default"
+		"kali-linux-light"
+		"kali-linux-arm"
+		"kali-linux-nethunter"
+		"Back"
+		)
+		Desktop=(
+		"kali-desktop-core"
+		"kali-desktop-e17"
+		"kali-desktop-gnome"
+		"kali-desktop-i3"
+		"kali-desktop-kde"
+		"kali-desktop-lxde"
+		"kali-desktop-mate"
+		"kali-desktop-xfce"
+		"Back"	
+		)
+		Tools=(
+		"kali-tools-gpu"
+		"kali-tools-hardware"
+		"kali-tools-crypto-stego"
+		"kali-tools-fuzzing"
+		"kali-tools-802-11"
+		"kali-tools-bluetooth"
+		"kali-tools-rfid"
+		"kali-tools-sdr"
+		"kali-tools-voip"
+		"kali-tools-windows-resources"
+		"Back"
+		)
+		CateTools=(
+		"kali-tools-information-gathering"
+		"kali-tools-vulnerability"
+		"kali-tools-web"
+		"kali-tools-database"
+		"kali-tools-passwords"
+		"kali-tools-wireless"
+		"kali-tools-reverse-engineering"
+		"kali-tools-exploitation"
+		"kali-tools-social-engineering"
+		"kali-tools-sniffing-spoofing"
+		"kali-tools-post-exploitation"
+		"kali-tools-forensics"
+		"kali-tools-reporting"	
+		"Back"
+		)
 	
-	On=1
-	Off=0
-	choice=0
-	until [ $choice == 255  ];
-       	do
-		createMenu ${Menu[@]} $On
-		choice=$?
+		On=1
+		Off=0
+		choice=0
+		until [ $choice == 255  ];
+       		do
+			createMenu ${Menu[@]} $On
+			choice=$?
 
-		if [[ $choice -eq 0  ]]; then
-			echo "Inside System!..."
-			createMenu ${System[@]} $Off 
-		elif [[ $choice -eq 1  ]]; then
-			createMenu ${Desktop[@]} $Off
-		elif [[ $choice -eq 2  ]]; then
-			createMenu ${Tools[@]} $Off
-		elif [[ $choice -eq 3  ]]; then
-			createMenu ${CateTools[@]} $Off
-		elif [[ $choice -eq 4  ]]; then
-			apt "kali-tools-top10"
-		elif [[ $choice -eq 5  ]]; then
-			apt "kali-linux-everything"
-		elif [[ $choice -eq 6  ]]; then
-			#Documentation for this Script
-			clear
-			cat README.md
-			read -p "Press any key to return to MainMenu..."
-		elif [[ $choice -eq 7  ]]; then
-			if [[ ${#AptArgs[@]} -eq 0  ]]; then
-				read -p "Select any package to INSTALL..."
-			else
-				apt ${AptArgs[@]}
+			if [[ $choice -eq 0  ]]; then
+				echo "Inside System!..."
+				createMenu ${System[@]} $Off 
+			elif [[ $choice -eq 1  ]]; then
+				createMenu ${Desktop[@]} $Off
+			elif [[ $choice -eq 2  ]]; then
+				createMenu ${Tools[@]} $Off
+			elif [[ $choice -eq 3  ]]; then
+				createMenu ${CateTools[@]} $Off
+			elif [[ $choice -eq 4  ]]; then
+				apt "kali-tools-top10"
+			elif [[ $choice -eq 5  ]]; then
+				apt "kali-linux-everything"
+			elif [[ $choice -eq 6  ]]; then
+				#Documentation for this Script
+				clear
+				cat README.md
+				read -p "Press any key to return to MainMenu..."
+			elif [[ $choice -eq 7  ]]; then
+				if [[ ${#AptArgs[@]} -eq 0  ]]; then
+					read -p "Select any package to INSTALL..."
+				else
+					apt ${AptArgs[@]}
+				fi
 			fi
-		fi
-	done
-	echo "bye bye | Sayonara...!"
+		done
+		echo "bye bye | Sayonara...!"
+
+	else
+		echo "Sorry...Need sudo..."
+
+	fi
 }
 main
